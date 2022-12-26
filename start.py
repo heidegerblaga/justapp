a716dd0170e37c8735948ad253faa74c9e2d1c80
heidegerblaga <skyrimpluta@gmail.com>
2022-12-26 11:00:09 +0100

    m

---
+++ start.py
@@ -0,0 +1,32 @@
+
+
+
+#if __name__=="__main__":
+
+def main():
+  for path in newimages():
+    print(path)
+    if "jpg" in path:
+
+      if session.query(Shopping).filter(Shopping.filename==path).first() == None:
+
+        pars(path)
+        try:
+          add = load(path)
+        except :
+          print("\n\n\nProbably you daily quota has exceed, try later.")
+          break
+
+
+        print('''Do you want add this to data base ?\n
+        1) yes \n
+        0) no \n
+        ''')
+        choice = input()
+
+        if int(choice)==1:
+            add_to_db(add[0],add[1],add[2],add[3])
+
+        else:
+            continue
+
